eb8c5658bc9769c320e23bc4b9467bdde17d6db0
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-04-01 02:43:52 +0200

    refactor: update index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export { Select, SelectButton, SelectOption, SelectOptionList } from "~/components/Select";
+export { Select, SelectButton, SelectLabel, SelectOption, SelectOptionList } from "~/components/Select";
 export { Menu, MenuButton, MenuItem, MenuItemList } from "~/components/Menu";
-export { Tabs } from "~/components/tabs/Tabs";
-export { TabsItem } from "~/components/tabs/TabsItem";
-export { TabsItemList } from "~/components/tabs/TabsItemList";
