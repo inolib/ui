1d8a1c2914d19928f10f3e3beb94e747cdcf4b45
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-04-03 04:44:29 +0200

    refactor: Menu component

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
+export { Menu, MenuButton, MenuItems, MenuItemLink } from "~/components/Menu";
 export { Select, SelectButton, SelectLabel, SelectOption, SelectOptionList } from "~/components/Select";
-export { Menu, MenuButton, MenuItem, MenuItemList } from "~/components/Menu";
